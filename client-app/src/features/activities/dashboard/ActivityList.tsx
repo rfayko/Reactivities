e88e23d58bd982a833c907c6c1973eee3a5245f2
rfayko <rfayko@raadsinc.com>
2019-09-08 15:40:59 -0400

    Section 5 Post 1

---
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Item, Button, Label, Segment } from "semantic-ui-react";
+import { IActivity } from "../../../app/models/activity";
+
+interface IProps {
+  activities: IActivity[];
+}
+
+const ActivityList: React.FC<IProps> = ({ activities }) => {
+  return (
+    <Segment clearing>
+      <Item.Group divided>
+        {activities.map(act => (
+          <Item key={act.id}>
+            <Item.Content>
+              <Item.Header as="a">{act.title}</Item.Header>
+              <Item.Meta>{act.date}</Item.Meta>
+              <Item.Description>
+                <div>{act.description}</div>
+                <div>{act.city}, {act.venue}</div>
+              </Item.Description>
+              <Item.Extra>
+                <Button floated="right" content="View" color="blue" />
+                <Label basic content={act.category} />
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        ))}
+      </Item.Group>
+    </Segment>
+  );
+};
+
+export default ActivityList;
